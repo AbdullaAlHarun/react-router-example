c57666a401f4100ec62f566a401c94fce8488f15
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-03-19 22:04:03 +0100

    Added Dynamic Route, Params And Load Dynamic Data!

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import Home from './componants/Home/Home';
 import About from './componants/About/About';
 import Contact from './componants/Contact/Contact';
 import Users from './componants/Users/Users.jsx';
+import Posts from './componants/Posts/Posts.jsx';
+import PostDetails from './componants/PostDetails/PostDetails.jsx';
 
 
 
@@ -31,6 +33,16 @@ const router = createBrowserRouter([
       {
         path: '/users',
         element: <Users></Users>
+      },
+      {
+        path: '/posts',
+        loader: () => fetch ('https://jsonplaceholder.typicode.com/posts'),
+        element: <Posts></Posts>
+      },
+      {
+        path: '/post/:postId',
+        loader: ({params}) => fetch (`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+        element: <PostDetails></PostDetails>
       }
 
     ]
