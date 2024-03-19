581b85288f9817d15fd5d39a0f08b2586544f465
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-03-19 22:12:09 +0100

    Go Back function Added!!

--- src/componants/Post/Post.jsx
+++ src/componants/Post/Post.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({post}) => {
     const {id, title} = post;
+
+const navigate = useNavigate();
+
     const postStyle = {
         border: '2px solid yellow',
         padding: '5px', 
         borderRadius: '20px'
     } 
+
+const handleShowDetails = () => {
+    navigate(`/post/${id}`);
+}
 
     return (
         <div style={postStyle}> 
@@ -14,6 +21,7 @@ const Post = ({post}) => {
             <p>Title: {title}</p>
            <Link to={`/post/${id}`}>Post Details</Link>
            <Link to={`/post/${id}`}><button>Show Details</button></Link>
+            <button onClick={handleShowDetails}>Click to see details</button>
         </div>
     );
 };
